8049f996ed2771f5f012c5bcc22938e1646b874a
Sunny Master <master.codeworks@gmail.com>
2024-09-22 12:29:24 -0400

    add route, views function, template for destination-index

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
+  path('destinations/', views.destination_index, name='destination-index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Destination
 
 # home view
 def home(request):
   return render(request, 'home.html')
+
+def destination_index(request):
+  destinations = Destination.objects.all()
+  return render(request, 'destinations/index.html', { 'destinations': destinations })
