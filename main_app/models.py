3a33e1e3de9596d4b20384afffc884d613ad6c44
Sunny Master <master.codeworks@gmail.com>
2024-09-22 20:36:55 -0400

    update Destination model; update styling for destination-detail

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 D_TYPES = (
@@ -22,7 +23,9 @@ class Destination(models.Model):
     choices=D_TYPES,
     default=D_TYPES[0][0]
   )
+  city = models.CharField(max_length=100, default='not known')
   country = models.CharField(max_length=100)
+  comment = models.TextField(max_length=250, default="no comments")
   rating = models.IntegerField(default=0)
   image_url = models.URLField(
     max_length=500,
@@ -33,3 +36,6 @@ class Destination(models.Model):
 
   def __str__(self):
     return self.name
+
+  def get_absolute_url(self):
+    return reverse('destination-detail', kwargs={'destination_id': self.id})
