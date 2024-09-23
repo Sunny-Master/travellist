98f0a82212ac6aec0f23ae23996d4c7a28a6da77
Sunny Master <master.codeworks@gmail.com>
2024-09-23 15:43:02 -0400

    add signup, login and logout functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('destinations/', views.DestinationList.as_view(), name='destination-index'),
   path('destinations/<int:pk>/', views.DestinationDetail.as_view(), name='destination-detail'),
   path('destinations/create/', views.DestinationCreate.as_view(), name='destination-create'),
   path('destinations/<int:pk>/update/', views.DestinationUpdate.as_view(), name='destination-update'),
   path('destinations/<int:pk>/delete/', views.DestinationDelete.as_view(), name='destination-delete'),
+  path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Destination
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
+# Sign Up
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in
+      login(request, user)
+      return redirect('destination-index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST request OR a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
 
 # home view
-def home(request):
-  return render(request, 'home.html')
+class Home(LoginView):
+  template_name = 'home.html'
 
 class DestinationList(ListView):
   model = Destination
