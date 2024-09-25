2753cbecad15ab7289b7ee9aef41d1022bbdca82
Sunny Master <master.codeworks@gmail.com>
2024-09-25 13:18:43 -0400

    update for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn travellist.wsgi
--- travellist/settings.py
+++ travellist/settings.py
@@ -14,6 +14,7 @@ import os
 env = environ.Env()
 from pathlib import Path
 import cloudinary
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,9 +28,10 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if not 'ON_HEROKU' in os.environ:
+    DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -45,10 +47,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -81,17 +85,27 @@ WSGI_APPLICATION = 'travellist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('NEON_DATABASE_NAME'),
-        'USER': env('NEON_USER'),
-        'PASSWORD': env('NEON_PASSWORD'),
-        'HOST': env('NEON_HOST'),
-        'PORT': '5432',
-        'OPTIONS': {'sslmode': 'require'}
+if 'ON_HEROKU' in os.environ:
+    DATABASES = {
+        "default": dj_database_url.config(
+            env='DATABASE_URL',
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True,
+        ),
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': env('NEON_DATABASE_NAME'),
+            'USER': env('NEON_USER'),
+            'PASSWORD': env('NEON_PASSWORD'),
+            'HOST': env('NEON_HOST'),
+            'PORT': '5432',
+            'OPTIONS': {'sslmode': 'require'}
+        }
+    }
 
 # Cloudinary configuration
 cloudinary.config(
@@ -135,6 +149,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'destination-index'
 LOGOUT_REDIRECT_URL = 'home'
