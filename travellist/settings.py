b87e89c0d43401cebef65e592ddf3896a8568358
Sunny Master <master.codeworks@gmail.com>
2024-09-25 16:42:26 -0400

    update settings.py to change STATICFILES_STORAGE

--- travellist/settings.py
+++ travellist/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'destination-index'
 LOGOUT_REDIRECT_URL = 'home'
