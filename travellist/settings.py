71ba313d270706876fa12f5907c4e4e9eb794663
Sunny Master <master.codeworks@gmail.com>
2024-09-26 09:49:45 -0400

    update for fixing bugs for deployment

--- travellist/settings.py
+++ travellist/settings.py
@@ -148,6 +148,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+WHITENOISE_MANIFEST_STRICT = False
+
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
