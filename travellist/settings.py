c0565f8dc2924991f0a46eecefb783f03ddb5b18
Sunny Master <master.codeworks@gmail.com>
2024-09-26 11:40:22 -0400

    update for fixing deployment

--- travellist/settings.py
+++ travellist/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'destination-index'
 LOGOUT_REDIRECT_URL = 'home'
