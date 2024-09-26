ed13d65ee7860dbf62957b96c6cb09979ddbb37d
Sunny Master <master.codeworks@gmail.com>
2024-09-26 13:59:59 -0400

    update requirements.txt to fix deployment

--- travellist/settings.py
+++ travellist/settings.py
@@ -148,12 +148,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-WHITENOISE_MANIFEST_STRICT = False
-
 
 LOGIN_REDIRECT_URL = 'destination-index'
 LOGOUT_REDIRECT_URL = 'home'
